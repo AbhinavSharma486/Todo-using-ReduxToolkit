15626c406572b968a4c8066029af552c6f4fb01b
Abhinav Sharma <abhinavparashar486@gmail.com>
2023-10-15 00:51:36 +0530

    todo-using-redux-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+import './app.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about Redux-Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from '../features/todo/todoSlice';
+
+const store = configureStore({
+    reducer: todoReducer
+})
+
+export default store;
